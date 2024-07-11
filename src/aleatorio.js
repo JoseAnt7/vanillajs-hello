0bb1faffef3b628f0abd4de611e28aa6fa9a5aa9
José Antonio Llorens Padilla <131461633+JoseAnt7@users.noreply.github.com>
2024-07-11 15:14:00 +0000

    plantilla

--- src/aleatorio.js
+++
@@ -1,4 +0,0 @@
-let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
-let action = ['ate', 'peed', 'crushed', 'broke'];
-let what = ['my homework', 'my phone', 'the car'];
-let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
