b72b6f29921cee2478dc02177af5eb5ba67e97fa
José Antonio Llorens Padilla <131461633+JoseAnt7@users.noreply.github.com>
2024-07-11 16:21:50 +0000

    casi completo

--- src/app.js
+++ src/app.js
@@ -7,5 +7,43 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+  let elemento;
+  let number;
+
+  for (let i = 0; i <= 3; i++) {
+    switch (i) {
+      case 0:
+        elemento = document.getElementById("who");
+        number = Math.floor(Math.random() * who.length);
+        elemento.innerHTML = who[number];
+        break;
+      case 1:
+        elemento = document.getElementById("action");
+        number = Math.floor(Math.random() * action.length);
+        elemento.innerHTML = action[number];
+        break;
+      case 2:
+        elemento = document.getElementById("what");
+        number = Math.floor(Math.random() * what.length);
+        elemento.innerHTML = what[number];
+        break;
+      case 3:
+        elemento = document.getElementById("when");
+        number = Math.floor(Math.random() * when.length);
+        elemento.innerHTML = when[number];
+        break;
+      default:
+        break;
+    }
+  }
 };
